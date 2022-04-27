87b6d10f258d730e33259339420880fe2789cbb6
David Angelo <s523035@nwmissouri.edu>
2022-04-27 15:48:15 -0500

    secure

--- routes/glintstone.js
+++ routes/glintstone.js
@@ -3,12 +3,6 @@ const glintstone_controllers = require('../controllers/glintstone');
 const glintstone = require('../models/glintstone');
 var router = express.Router();
 
-router.get('/', secured, glintstone_controllers.glintstone_view_all_Page);
-router.get('/detail', secured, glintstone_controllers.glintstone_view_one_Page);
-router.get('/create', secured, glintstone_controllers.glintstone_create_Page);
-router.get('/update', secured, glintstone_controllers.glintstone_update_Page);
-router.get('/delete', secured, glintstone_controllers.glintstone_delete_Page);
-
 
 const secured = (req, res, next) => {
     if (req.user) {
@@ -17,5 +11,13 @@ const secured = (req, res, next) => {
     req.session.returnTo = req.originalUrl;
     res.redirect("../login");
 }
+
+router.get('/', secured, glintstone_controllers.glintstone_view_all_Page);
+router.get('/detail', secured, glintstone_controllers.glintstone_view_one_Page);
+router.get('/create', secured, glintstone_controllers.glintstone_create_Page);
+router.get('/update', secured, glintstone_controllers.glintstone_update_Page);
+router.get('/delete', secured, glintstone_controllers.glintstone_delete_Page);
+
+
 
 module.exports = router;
