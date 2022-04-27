035d496cf5a533763a75cdfdb5af1fc459645e07
David Angelo <s523035@nwmissouri.edu>
2022-04-27 15:26:26 -0500

    security stuff

--- routes/glintstone.js
+++ routes/glintstone.js
@@ -3,11 +3,19 @@ const glintstone_controllers = require('../controllers/glintstone');
 const glintstone = require('../models/glintstone');
 var router = express.Router();
 
-router.get('/', glintstone_controllers.glintstone_view_all_Page);
-router.get('/detail', glintstone_controllers.glintstone_view_one_Page);
-router.get('/create', glintstone_controllers.glintstone_create_Page);
-router.get('/update', glintstone_controllers.glintstone_update_Page);
-router.get('/delete', glintstone_controllers.glintstone_delete_Page);
+router.get('/', secured, glintstone_controllers.glintstone_view_all_Page);
+router.get('/detail', secured, glintstone_controllers.glintstone_view_one_Page);
+router.get('/create', secured, glintstone_controllers.glintstone_create_Page);
+router.get('/update', secured, glintstone_controllers.glintstone_update_Page);
+router.get('/delete', secured, glintstone_controllers.glintstone_delete_Page);
 
+
+const secured = (req, res, next) => {
+    if (req.user) {
+        return next();
+    }
+    req.session.returnTo = req.originalUrl;
+    res.redirect("../login");
+}
 
 module.exports = router;
